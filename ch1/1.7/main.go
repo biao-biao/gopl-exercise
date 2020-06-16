ccd14aa28e1524a886843d69ac6318c607f7f433
Biao Wu <wubiao1991@hotmail.com>
2020-06-16 23:52:14 +0800

    add exercise 1.7

---
+++ ch1/1.7/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		// https://stackoverflow.com/questions/41574028/golang-no-new-variables-on-left-side-of-while-a-similar-one-didnt-occur-th/41574085
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
