9ab98418f878e34f6738854c8cb32058bf994277
Wu Biao - Online Linux Administrator - APAC - SHA <bwu@uco-pdc-mgmt01.emea-west-pdc-onl.uc.internal>
2020-06-26 23:54:23 +0800

    rename chapter

---
+++ ch03/3.4/README.md
@@ -0,0 +1,1 @@
+benchmark two surface functions
---
+++ ch03/3.4/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"net/http"
+	"os"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func main() {
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		//!+http
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			if err := r.ParseForm(); err != nil {
+				log.Print(err)
+			}
+
+			w.Header().Set("Content-type", "image/svg+xml")
+			surface(w)
+		}
+		http.HandleFunc("/", handler)
+		//!-http
+		log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
+		return
+	}
+	surface(os.Stdout)
+}
+
+func surface(out io.Writer) {
+	fmt.Fprintf(out, "<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>\n", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+			if math.IsNaN(ax + ay + bx + by + cx + cy + dx + dy) {
+				continue
+			}
+			fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Fprintln(out, "</svg>")
+}
+
+// func surface(out io.Writer) {
+// 	s := fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
+// 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+// 		"width='%d' height='%d'>\n", width, height)
+// 	for i := 0; i < cells; i++ {
+// 		for j := 0; j < cells; j++ {
+// 			ax, ay := corner(i+1, j)
+// 			bx, by := corner(i, j)
+// 			cx, cy := corner(i, j+1)
+// 			dx, dy := corner(i+1, j+1)
+// 			if math.IsNaN(ax + ay + bx + by + cx + cy + dx + dy) {
+// 				continue
+// 			}
+// 			s += fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+// 				ax, ay, bx, by, cx, cy, dx, dy)
+// 		}
+// 	}
+// 	s += fmt.Sprintln("</svg>")
+// 	fmt.Fprint(out, s)
+// }
+
+func corner(i, j int) (float64, float64) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z := f(x, y)
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance from (0,0)
+	return math.Sin(r) / r
+}
+
+//!-
