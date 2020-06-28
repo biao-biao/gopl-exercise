d147faa451813cfd598d260b5b1eb0e0c539009a
Wu Biao - Online Linux Administrator - APAC - SHA <bwu@uco-pdc-mgmt01.emea-west-pdc-onl.uc.internal>
2020-06-28 21:44:03 +0800

    add exercise 4.3

---
+++ ch04/4.3/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [...]int{0, 1, 2, 3, 4, 5}
+	reverse(&arr)
+	fmt.Println(arr)
+}
+
+func reverse(p *[6]int) {
+	for i, j := 0, len(p)-1; i < j; i, j = i+1, j-1 {
+		p[i], p[j] = p[j], p[i]
+	}
+}
