773838a4024e75907d984d59b649b5aa89be177a
Wu Biao - Online Linux Administrator - APAC - SHA <bwu@uco-pdc-mgmt01.emea-west-pdc-onl.uc.internal>
2020-06-29 12:17:59 +0800

    add exercise 4.4

---
+++ ch04/4.4/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := [...]int{1, 2, 3, 4, 5}
+	rotate(s[:], 7)
+	fmt.Println(s)
+}
+
+func rotate(s []int, n int) {
+	var l []int
+	for i := range s {
+		l = append(l, s[(n+i)%len(s)])
+	}
+	copy(s, l)
+}
