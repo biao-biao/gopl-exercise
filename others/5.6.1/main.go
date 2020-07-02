4676ca619046d2df4b1ee085cf2a0f8436e31865
Wu Biao - Online Linux Administrator - APAC - SHA <bwu@uco-pdc-mgmt01.emea-west-pdc-onl.uc.internal>
2020-07-02 22:39:23 +0800

    add others folder

--- others/5.6.1/main.go
+++ others/5.6.1/main.go
@@ -1,18 +1,22 @@
+// See page 141.
+// and the quivalent sample in python https://www.toptal.com/python/interview-questions
+
 package main
 
 import "fmt"
 
-// https://www.toptal.com/python/interview-questions
 func main() {
+	var arr []int
 	for _, f := range m() {
-		fmt.Println(f(2))
+		arr = append(arr, f(2))
 	}
-	a := []string{"a", "b"}
-	fmt.Printf("%T\n", a)
+
+	fmt.Println(arr) // [8 8 8 8]
 }
 
 func m() (arr []func(int) int) {
 	for i := 0; i < 4; i++ {
+		// i := i
 		f := func(x int) int {
 			return i * x
 		}
